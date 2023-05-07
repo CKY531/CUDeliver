27281e78e8918b9de02b9d4a2aab97f031fefeb6
CKY531 <69076275+CKY531@users.noreply.github.com>
2023-05-07 16:57:09 +0800

    Add bottom nav bar

---
+++ app/src/main/java/edu/cuhk/cudeliver/OrderActivity.java
@@ -0,0 +1,53 @@
+package edu.cuhk.cudeliver;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import edu.cuhk.cudeliver.databinding.ActivityOrderBinding;
+
+public class OrderActivity extends AppCompatActivity {
+
+    ActivityOrderBinding orderBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        orderBinding = ActivityOrderBinding.inflate(getLayoutInflater());
+        setContentView(orderBinding.getRoot());
+        replaceFrag(new OrderDisplayFragment());
+
+        //Add Listener to listen to bottom nav bar
+        orderBinding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()) {
+                case R.id.create:
+                    replaceFrag(new OrderCreateFragment());
+                    break;
+                case R.id.orderList:
+                    replaceFrag(new OrderDisplayFragment());
+                    break;
+                case R.id.yourOrder:
+                    replaceFrag(new YourOrderFragment());
+                    break;
+                case R.id.orderToDeliver:
+                    replaceFrag(new OrderToDeliverFragment());
+                    break;
+            }
+
+            return true;
+        });
+    }
+
+    //Replace old order fragment with the new one according to the selected item in bottom nav
+    public void replaceFrag(Fragment frag) {
+        FragmentManager fMan = getSupportFragmentManager();
+        FragmentTransaction fTran = fMan.beginTransaction();
+        fTran.replace(R.id.orderFragLayout, frag);
+        fTran.commit();
+    }
+
+}
